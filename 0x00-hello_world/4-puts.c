6b71d073bc012713fdf96a5cfbc1541ac2a00962
Abdulrahman <abdulrahmanabubakar34@yahoo.com>
2022-12-08 23:19:24 +0100

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
