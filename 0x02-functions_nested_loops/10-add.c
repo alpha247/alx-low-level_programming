6422ee733f617e2edfbc30d1d2c41241c203ab6e
Abdulrahman <abdulrahmanabubakar34@yahoo.com>
2022-12-14 10:57:42 +0100

    correction

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,16 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * add - Check Holberton
- * @a: An integer a
- * @b: An integer b
- * Description: Function that adds two integers and returns the result
- * Return: Result to add a and b
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int add(int a, int b)
+int main(void)
 {
-	return (a + b);
+  int n;
+
+  n = add(89, 9);
+  printf("%d\n", n);
+  return (0);
 }
